fd38f4dda298c0e6743f43dcb79b07e1ba8690b2
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-04-15 14:18:02 +0200

    Create the addBookPage

---
+++ app/ReadME/AddBookPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace ReadME;
+
+public partial class AddBookPage : ContentPage
+{
+	public AddBookPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnAddBookClicked(object sender, EventArgs e)
+    {
+
+    }
+}
