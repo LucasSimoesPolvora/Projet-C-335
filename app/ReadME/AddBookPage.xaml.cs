6fce7965917a30a6b9bec44641025924bb8f5488
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-04-22 13:41:28 +0200

    Do a try catch to fix a bug (doesnt work)

--- app/ReadME/AddBookPage.xaml.cs
+++ app/ReadME/AddBookPage.xaml.cs
@@ -1,4 +1,6 @@
 
+using System.Diagnostics;
+
 namespace ReadME;
 
 public partial class AddBookPage : ContentPage
@@ -17,23 +19,29 @@ public partial class AddBookPage : ContentPage
         {
             await Permissions.RequestAsync<Permissions.Media>();
         }*/
+        try
+        {
+            var book = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Choose the book you want to add",
+                FileTypes = null
+            });
+            // If the filepicker gets nothing, return
+            if (book == null)
+                return;
 
-        var book = await FilePicker.PickAsync(new PickOptions
+            // If the file isn't an epub, returns an error 
+            if (!book.FileName.EndsWith("epub"))
+            {
+                await Shell.Current.DisplayAlert("Error", "The selected file extension is not supported", "OK");
+                return;
+            }
+            await Shell.Current.DisplayAlert("Done", "The selected file was successfully imported", "OK");
+        } catch(Exception ex)
         {
-            PickerTitle = "Choose the book you want to add",
-            FileTypes = null
-        });
-        // If the filepicker gets nothing, return
-        if (book == null)
-            return;
-        
-        // If the file isn't an epub, returns an error 
-        if (!book.FileName.EndsWith("epub"))
-        {
-            await Shell.Current.DisplayAlert("Error", "The selected file extension is not supported", "OK");
-            return;
+            Trace.Write(ex.ToString());
         }
-        await Shell.Current.DisplayAlert("Done", "The selected file was successfully imported", "OK");
+        
 
     }
 }
